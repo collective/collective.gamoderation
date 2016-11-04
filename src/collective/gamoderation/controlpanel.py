8d76c2b1e9147ccdf6f54eaaba0d31d0a8573734
Joshua LaPlace <jlaplace@enfoldsystems.com>
2016-11-04 12:26:52 -0500

    Control panel fixes.

--- src/collective/gamoderation/controlpanel.py
+++ src/collective/gamoderation/controlpanel.py
@@ -4,8 +4,8 @@ from zope.interface import Interface
 from zope.interface import implements
 
 from plone.autoform.form import AutoExtensibleForm
+from z3c.form import button
 from z3c.form.form import EditForm
-from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 from collective.gamoderation.interfaces import IAnalyticsModeration
 
 from collective.gamoderation.widgets import SelectSequenceFieldWidget
@@ -14,6 +14,15 @@ from collective.gamoderation.widgets import BlockResultsFieldWidget
 from collective.gamoderation import _
 
 from plone.app.registry.browser.controlpanel import ControlPanelFormWrapper
+
+from Products.statusmessages.interfaces import IStatusMessage
+
+from zope.component.hooks import getSite
+
+from zope.i18nmessageid import MessageFactory
+
+_plone = MessageFactory('plone')
+
 
 class IAnalyticsModerationControlPanelForm(Interface):
     """
@@ -52,25 +61,25 @@ class AnalyticsModerationControlPanelForm(AutoExtensibleForm, EditForm):
             return self.request.response.redirect(analytics_tool)
         else:
             reload_view = False
-            if ('form.moderated_channels.select' in self.request.form or
+            if ('form.widgets.moderated_channels.select' in self.request.form or
                     'form.actions.save' in self.request.form):
                 moderated_channel = self.request.form.get(
-                    'form.moderated_channels')
+                    'form.widgets.moderated_channels')
                 if moderated_channel:
                     self.request.set('moderated_channel', moderated_channel)
                 else:
                     reload_view = True
 
-            if 'form.moderated_channels.remove' in self.request.form:
+            if 'form.widgets.moderated_channels.remove' in self.request.form:
                 moderated_channel = self.request.form.get(
-                    'form.moderated_channels')
+                    'form.widgets.moderated_channels')
                 adapter = IAnalyticsModeration(self.context)
                 adapter.remove_channel(moderated_channel)
                 reload_view = True
 
-            if 'form.moderated_channels.add' in self.request.form:
+            if 'form.widgets.moderated_channels.add' in self.request.form:
                 moderated_channel = self.request.form.get(
-                    'form.moderated_channels.new_value')
+                    'form.widgets.moderated_channels.new_value')
                 adapter = IAnalyticsModeration(self.context)
                 adapter.add_channel(moderated_channel)
                 reload_view = True
@@ -82,6 +91,33 @@ class AnalyticsModerationControlPanelForm(AutoExtensibleForm, EditForm):
             else:
                 return super(
                     AnalyticsModerationControlPanelForm, self).__call__()
+
+    def updateActions(self):
+        super(AnalyticsModerationControlPanelForm, self).updateActions()
+        self.actions['save'].addClass("context")
+        self.actions['cancel'].addClass("standalone")
+
+    @button.buttonAndHandler(_(u'Save'), name='save')
+    def handleSave(self, action):
+        data, errors = self.extractData()
+        if errors:
+            self.status = self.formErrorsMessage
+            return
+        self.applyChanges(data)
+        IStatusMessage(self.request).addStatusMessage(
+            _plone(u'Changes saved.'),
+            'info')
+        self.request.response.redirect(self.request.getURL())
+
+    @button.buttonAndHandler(_plone(u'Cancel'), name='cancel')
+    def handleCancel(self, action):
+        IStatusMessage(self.request).addStatusMessage(
+            _plone(u'Changes canceled.'),
+            'info')
+        self.request.response.redirect(u'{0}/{1}'.format(
+            getSite().absolute_url(),
+            '@@overview-controlpanel'
+        ))
 
 
 class AnalyticsModerationControlPanel(ControlPanelFormWrapper):
